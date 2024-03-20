4d1544bbec48963f98661b5777316f5fb3aca5b5
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-03-20 16:36:07 +0300

    added solutions on even numbers, map tree

---
+++ MapTree/MapTree.Tests/UnitTest1.fs
@@ -0,0 +1,12 @@
+﻿module MapTree.Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty tree case`` () =
+    Assert.Pass()
+
+[<Test>]
+let ``casual case test`` () =
+    Assert.Pass()
---
+++ MapTree/MapTree/MapTree.fs
@@ -0,0 +1,12 @@
+﻿namespace MapTree
+
+module MapTree =
+
+    type Tree<'a> = 
+        | Tree of 'a * Tree<'a> * Tree<'a>
+        | Leaf of 'a
+    
+    let rec mapTree tree f = 
+        match tree with
+        | Tree (x, l, r) -> Tree ( f x, mapTree l f, mapTree r f)
+        | Leaf x -> Leaf (f x)
