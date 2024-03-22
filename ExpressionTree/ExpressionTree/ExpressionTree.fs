a9ad8cb026b385d43c3665256fa4812983f4e059
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-03-22 20:22:47 +0300

    added solution on expression tree

---
+++ ExpressionTree/ExpressionTree/ExpressionTree.fs
@@ -0,0 +1,16 @@
+﻿type Operation = Addition | Substraction | Multiplication | Division
+
+type ExpressionTree =
+    | ExpressionTree of Operation * ExpressionTree * ExpressionTree
+    | Leaf of int 
+
+let rec countTree tree = 
+    match tree with 
+        | Leaf n -> n
+        | ExpressionTree(Addition, l, r) -> countTree l + countTree r
+        | ExpressionTree(Substraction, l, r) -> countTree l - countTree r
+        | ExpressionTree(Multiplication, l, r) -> countTree l * countTree r
+        | ExpressionTree(Division, l, r) ->  
+            let right = countTree r
+            if right = 0 then raise (System.DivideByZeroException("DivisionByZero"))
+            else countTree l / right
