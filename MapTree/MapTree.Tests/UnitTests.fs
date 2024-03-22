ef6e79f829bc99c31daf9d1fbc1b90f9e044dfaa
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-03-22 19:42:48 +0300

    added tests to map tree

---
+++ MapTree/MapTree.Tests/UnitTests.fs
@@ -0,0 +1,23 @@
+﻿module MapTree.Tests
+
+open NUnit.Framework
+open FsUnit
+open MapTree.MapTree
+
+let ``test case 1`` =
+    seq {
+        TestCaseData(Tree(4, Tree(2, Leaf(1), Leaf(3)), Tree(6, Leaf(5), Leaf(7))), Tree(5, Tree(3, Leaf(2), Leaf(4)), Tree(7, Leaf(6), Leaf(8))))
+    }
+
+[<TestCaseSource("test case 1")>]
+let ``test 1`` (tree : Tree<int>, answer : Tree<int>) =
+    mapTree tree (fun x -> x + 1) |> should equal answer
+
+let ``test case 2`` =
+    seq {
+        TestCaseData(Tree("a", Tree("b", Leaf("c"), Leaf("d")), Tree("e", Leaf("f"), Leaf("g"))), Tree("aa", Tree("bb", Leaf("cc"), Leaf("dd")), Tree("ee", Leaf("ff"), Leaf("gg"))))
+    }
+
+[<TestCaseSource("test case 2")>]
+let ``test 2`` (tree : Tree<string>, answer : Tree<string>) =
+    mapTree tree (fun x ->  x + x) |> should equal answer
--- MapTree/MapTree/MapTree.fs
+++ MapTree/MapTree/MapTree.fs
@@ -5,8 +5,10 @@ module MapTree =
     type Tree<'a> = 
         | Tree of 'a * Tree<'a> * Tree<'a>
         | Leaf of 'a
-    
-    let rec mapTree tree f = 
+
+
+
+    let rec mapTree tree f =
         match tree with
-        | Tree (x, l, r) -> Tree ( f x, mapTree l f, mapTree r f)
-        | Leaf x -> Leaf (f x)
+        | Tree(x, l, r) -> Tree(f x, mapTree l f, mapTree r f)
+        | Leaf(x) -> Leaf(f x)       
