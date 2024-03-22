b24782d68706a6ab85afb99a7d20524ffd2dadf8
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-03-22 16:11:48 +0300

    added tests to even numbers

---
+++ EvenNumbers/EvenNumbers.Tests/UnitTests.fs
@@ -0,0 +1,35 @@
+﻿module EvenNumbers.Tests
+
+open NUnit.Framework
+open FsUnit
+open EvenNumbers.СountEvenNumbers
+
+let testCases =
+    seq {
+        yield (TestCaseData([1; 2; 3; 4; 5; 6; 7; 8], 4))
+        yield (TestCaseData([-1; -2; -3; -4; -5; -6; -7; -8], 4))
+    }
+
+[<TestCaseSource("testCases")>]
+let ``Test assert work correctly map`` (list : List<int>, answer : int) =
+    countEvenMap list |> should equal answer
+
+[<TestCaseSource("testCases")>]
+let ``Test assert work correctly fold`` (list : List<int>, answer : int) =
+    countEvenFold list |> should equal answer
+
+[<TestCaseSource("testCases")>]
+let ``Test assert work correctly filter`` (list : List<int>, answer : int) =
+    countEvenFilter list |> should equal answer
+
+[<Test>]
+let ``Test empty case map`` () =
+    countEvenMap [] |> should equal 0
+
+[<Test>]
+let ``Test empty case fold`` () =
+    countEvenFold [] |> should equal 0
+
+[<Test>]
+let ``Test empty case filter`` () =
+    countEvenFilter [] |> should equal 0
--- EvenNumbers/EvenNumbers/EvenNumbers.fs
+++ EvenNumbers/EvenNumbers/EvenNumbers.fs
@@ -6,6 +6,6 @@ module СountEvenNumbers =
 
     let countEvenFilter list =  list |> Seq.filter (fun x -> x % 2 = 0) |> Seq.length
 
-    let countEvenMap list = list |> Seq.map (fun x -> (1 - x % 2)) |> Seq.sum
+    let countEvenMap list = list |> Seq.map (fun x -> (1 - Math.Abs(x % 2))) |> Seq.sum
 
     let countEvenFold list = list |> Seq.fold (fun sum element -> (+) sum <| Math.Abs (element + 1) % 2) 0
