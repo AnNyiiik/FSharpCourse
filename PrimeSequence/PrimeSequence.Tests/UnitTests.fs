159ea55b415436c71596c6d5e05908b0ed9d63a8
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-03-23 14:39:25 +0300

    added solution on prime numbers

---
+++ PrimeSequence/PrimeSequence.Tests/UnitTests.fs
@@ -0,0 +1,10 @@
+﻿module PrimeSequence.Tests
+
+open NUnit.Framework
+open FsUnit
+open PrimeSequenceGenerator
+
+[<Test>]
+let ``compare first 10 numbers`` () =
+    let correctAnswer = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29]
+    generatePrimes () |> Seq.take 10 |> Seq.toList |> should equal correctAnswer
---
+++ PrimeSequence/PrimeSequence/PrimeSequence.fs
@@ -0,0 +1,12 @@
+﻿namespace PrimeSequence
+
+open System
+
+module PrimeSequenceGenerator =
+    let generatePrimes () = 
+        let isPrimeNumber n =
+            let bound = n |> float |> Math.Sqrt  |> Math.Round |> bigint |> (+) (bigint 1)
+            let rec checkDelimeters delimeter =
+                delimeter >= bound || (n % (int delimeter) <> 0 && checkDelimeters (delimeter + bigint 1))
+            checkDelimeters (bigint 2)
+        seq {yield! Seq.filter (fun number -> isPrimeNumber number) (Seq.initInfinite (fun i -> i + 2))}
