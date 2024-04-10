dfdf154189253bcdb021caa628522874c13d077e
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-04-11 00:12:32 +0300

    added test cases

--- Brackets/BracketsChecker.Tests/UnitTest.fs
+++ Brackets/BracketsChecker.Tests/UnitTest.fs
@@ -8,7 +8,19 @@ let testCasesTrue =
     seq {
         yield (TestCaseData("[][{}([][(){()()}])]"))
     }
+
+let testCasesFalse =
+    seq {
+        yield (TestCaseData("[]{{}}}"))
+        yield (TestCaseData("[{(})]"))
+        yield (TestCaseData("))(("))
+    }
 [<TestCaseSource("testCasesTrue")>]
 let ``Test cases true`` (sequence : string) =
     let res = checkSequence sequence
-    checkSequence sequence |> should equal true
+    Assert.That(checkSequence sequence)
+
+[<TestCaseSource("testCasesFalse")>]
+let ``Test cases false`` (sequence : string) =
+    let res = checkSequence sequence
+    Assert.That(not (checkSequence sequence))
