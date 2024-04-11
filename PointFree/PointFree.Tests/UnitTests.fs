9aca9422227ff763065973fc5feb0971f3a3c1d1
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-04-11 12:17:05 +0300

    added tests to point free

---
+++ PointFree/PointFree.Tests/UnitTests.fs
@@ -0,0 +1,11 @@
+﻿module PointFree.Tests
+
+open FsUnit
+open FsCheck
+open NUnit.Framework
+open PointFree.PointFree
+
+[<Test>]
+let ``Ckeck if the funcs the same`` () =
+    let isTheSame x l = func x l = funcWithoutLX x l
+    Check.QuickThrowOnFailure isTheSame
