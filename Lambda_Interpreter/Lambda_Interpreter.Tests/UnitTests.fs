d0726ebd81865756697ae438b6e119d4e8f3b022
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-04-11 23:35:48 +0300

    added tests

---
+++ Lambda_Interpreter/Lambda_Interpreter.Tests/UnitTests.fs
@@ -0,0 +1,36 @@
+﻿module Lambda_Interpreter.Tests
+
+open NUnit.Framework
+
+open Lambda_Interpreter.lambdaInterpreter
+
+
+[<Test>]
+let ``Evaluate Identity Function`` () =
+    let term = Abstraction ("x", Variable "x")
+    let expected = term
+    let result = normalize term
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``Evaluate Constant Function`` () =
+    let term = Abstraction ("x", Abstraction ("y", Variable "x"))
+    let expected = term
+    let result = normalize term
+    Assert.AreEqual(expected, result)
+[<Test>]
+let ``Evaluate Application`` () =
+    let term = Application (Abstraction ("x", Variable "x"), Variable "y")
+    let expected = Variable "y"
+    let result = normalize term
+    Assert.AreEqual(expected, result)
+[<Test>]
+let ``Evaluate Complex Function`` () =
+    let term =
+        Application (
+            Abstraction ("f", Application (Variable "f", Variable "x")),
+            Abstraction ("x", Variable "x")
+        )
+    let expected = Variable "x"
+    let result = normalize term
+    Assert.AreEqual(expected, result)
