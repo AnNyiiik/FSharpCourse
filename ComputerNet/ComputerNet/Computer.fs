4a0749684fea6b9acb32b91b1db1f9b3d4ed44fd
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-04-26 15:22:38 +0300

    changed

---
+++ ComputerNet/ComputerNet/Computer.fs
@@ -0,0 +1,44 @@
+﻿namespace ComputerNet
+
+open System
+
+module Computer = 
+
+    type Computer (macAdress : int, OS : string,
+        infectionProbability : float) =
+
+        member this.macAdress = macAdress
+
+        member val OS = OS with get
+
+        member val infectionProbability = infectionProbability with get
+
+        member val isDeseased = false with get
+
+        member val neighbours = [] with get
+
+        static member random = new System.Random()
+
+        member computer.addNeighbour (neighbour : Computer) =
+            neighbour :: computer.neighbours
+
+        member computer.getDeseased () =
+
+            let number = Computer.random.Next()
+
+            let isDeseased = infectionProbability |> (*) 100.0 |> int |> (-) number |> (>) 0
+
+            computer.isDeseased = isDeseased
+
+        member computer.SpreadVirus () =
+
+            let mutable getDeseased = false
+
+            List.iter (fun (neighbour : Computer) -> (getDeseased <- neighbour.getDeseased())) computer.neighbours
+
+        member computer.checkIfAllNeighboursAreDeseased () =
+
+            let mutable result = true
+
+            List.forall (fun (neigbour : Computer) ->
+                (neigbour.isDeseased || neigbour.infectionProbability = 0.0)) computer.neighbours
---
+++ ComputerNet/ComputerNet/Net.fs
@@ -0,0 +1,35 @@
+﻿namespace ComputerNet
+
+open System 
+open ComputerNet.Computer
+
+type Net (computers : Computer list, adjacencyMatrix : int[][], OS : Map<string, int>) =
+
+    let computers = computers
+
+    let adjacencyMatrix = adjacencyMatrix
+
+    do
+        for i = 0 to computers.Length - 1 do
+            for j = 0 to computers.Length - 1 do
+                if adjacencyMatrix.[i].[j] = 1 then
+                    computers.[i].addNeighbour(computers.[j]) |> ignore
+
+
+    member val IsAllComputersDeseased = false with get
+
+    member this.GetDeseasedComputers () =
+
+        List.filter (fun (computer : Computer) -> computer.isDeseased) computers
+
+    member this.SpreadVirus () =
+
+        if this.IsAllComputersDeseased then true
+        else
+            if (List.length (this.GetDeseasedComputers()) = List.length computers) then
+                true
+            else
+                let isAllNeigboursDeseased = true
+                let deseased = this.GetDeseasedComputers()
+                List.iter (fun (computer : Computer) -> computer.SpreadVirus()) deseased
+                List.forall (fun (computer : Computer) -> computer.checkIfAllNeighboursAreDeseased()) deseased
