162846ba2a39863e4ef8488d15f44e5ac31b31e9
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-05-31 11:41:14 +0300

    added odd file

---
+++ HW_6/HW_6/RounderBuilder.fs
@@ -0,0 +1,9 @@
+﻿module Rounder
+
+open System
+
+type RounderBuilder(accuracy : int) =
+    member this.Bind(x : float, func) =
+        func (Math.Round(x, accuracy))
+
+    member this.Return(x : float) = Math.Round(x, accuracy)
