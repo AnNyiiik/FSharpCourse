3a19a46a29e19c72cd7855d17e57e92d4ab14260
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-05-31 22:10:28 +0300

    changed power list

--- HW_1/HW_1/Program.fs
+++ HW_1/HW_1/Program.fs
@@ -1,56 +1,54 @@
-﻿//let factorial x =
-//    let rec factorialReq acc x = 
-//        if x = 1 || x = 0 then acc 
-//        else 
-//            factorialReq 
-//                (acc * x)
-//                (x - 1)
-//    factorialReq 1 x
+let factorial x =
+    let rec factorialReq acc x = 
+        if x = 1 || x = 0 then acc 
+        else 
+            factorialReq 
+                (acc * x)
+                (x - 1)
+    factorialReq 1 x
 
-//let fibonachi n =
-//    let rec fibonachiReq n1 n2 n = 
-//        if n = 0 then n1
-//        else if n = 1 then n2
-//        else
-//            fibonachiReq
-//                (n2) 
-//                (n1 + n2)
-//                (n - 1)
-//    fibonachiReq 0 1 n
+let fibonachi n =
+    let rec fibonachiReq n1 n2 n = 
+        if n = 0 then n1
+        else if n = 1 then n2
+        else
+            fibonachiReq
+                (n2) 
+                (n1 + n2)
+                (n - 1)
+    fibonachiReq 0 1 n
 
-let rec reverse list_new list_old = 
-    if List.isEmpty list_old then list_new
-    else
-        let it = List.head list_old
-        reverse (it :: list_new) (List.tail list_old)
+let reverse l =
+    let rec reverseReq list_new list_old = 
+        if List.isEmpty list_old then list_new
+        else
+            let it = List.head list_old
+            reverseReq (it :: list_new) (List.tail list_old)
+    reverseReq [] l
     
-let rec power n = 
-    if n = 0 then 1
-    else if n % 2 = 0 then 
-        let a = power (n / 2)
-        a * a
-    else 
-        let a = power (n - 1)
-        2 * a
+let makeListOfPowers n m =
 
-let rec makeListOfPowers n m i acc list = 
-    if n > m then [] 
-    else if i = m - n then list
-    else 
-        let acc = acc * 2
-        makeListOfPowers 
-            (n) 
-            (m) 
-            (i + 1) 
-            (acc) 
-            (acc :: list)
+    let rec power n = 
+        if n = 0 then 1
+        else if n % 2 = 0 then 
+            let a = power (n / 2)
+            a * a
+        else 
+            let a = power (n - 1)
+            2 * a
 
-let first = power 3
-let result = makeListOfPowers 7 6 0 first [first]
-let result_reveresed = reverse [] result 
-printfn "%A" result_reveresed
+    let rec makeListOfPowersReq n i acc list = 
+        if i > n then list
+        else
+            makeListOfPowersReq n (i + 1) (acc * 2) (acc :: list)
 
-//let rec findElement x pos list = 
-//    if List.length list = 0 then -1
-//    else if List.head list = x then pos
-//    else findElement (x) (pos + 1) (List.tail list)
+    let first = power n
+    let result  = makeListOfPowersReq (n + m) n first []
+    reverse result
+
+let findElement x list =
+    let rec findElementReq x pos list = 
+        if List.length list = 0 then -1
+        else if List.head list = x then pos
+        else findElementReq (x) (pos + 1) (List.tail list)
+    findElementReq x 0 list
