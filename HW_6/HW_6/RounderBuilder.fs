2b9dda3f3e1ed972a417faad23c3f6a922cdee6d
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-05-31 11:58:41 +0300

    changed rounder

--- HW_6/HW_6/RounderBuilder.fs
+++ HW_6/HW_6/RounderBuilder.fs
@@ -4,6 +4,6 @@ open System
 
 type RounderBuilder(accuracy : int) =
     member this.Bind(x : float, func) =
-        func x
+        func (Math.Round(x, accuracy))
 
     member this.Return(x : float) = Math.Round(x, accuracy)
