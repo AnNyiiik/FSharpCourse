baae4836833b7c754d65ff0467aed375c3422977
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-05-31 11:22:56 +0300

    changed point free

--- PointFree/PointFree.Tests/UnitTests.fs
+++ PointFree/PointFree.Tests/UnitTests.fs
@@ -3,9 +3,15 @@
 open FsUnit
 open FsCheck
 open NUnit.Framework
-open PointFree.PointFree
+
+let funcInitial x l = List.map (fun y -> y * x) l
+let funcWithoutL1 x = List.map (fun y -> y * x)
+let funcWithoutL2 x = List.map (fun y -> (*) y x)
+let funcWithoutL3 x = List.map << (fun y -> (*) y) <| x
+let funcWithoutLX = List.map << (fun y -> (*) y)
+let funcPointFree = List.map << (*)
 
 [<Test>]
 let ``Ckeck if the funcs the same`` () =
-    let isTheSame x l = func x l = funcWithoutLX x l
+    let isTheSame x l = funcInitial x l = funcPointFree x l
     Check.QuickThrowOnFailure isTheSame
--- PointFree/PointFree/PointFree.fs
+++ PointFree/PointFree/PointFree.fs
@@ -1,14 +1,5 @@
 ﻿namespace PointFree
 
 module PointFree =
-    let func x l = List.map (fun y -> y * x) l
-    let funcWithoutL x = List.map (fun y -> y * x)
-    let funcWithoutL2 x =
-        List.map ( (*) x << id )
-    let funcWithoutLX : int -> int list -> int list =
-        let f (x : int) = (*) x
-        let f_withoutX = f
-        let func  =  f_withoutX >> List.map
-        func
     
          
