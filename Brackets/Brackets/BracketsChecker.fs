6ce9bab1f60eef34931897ef727ed92fd637b838
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-06-01 02:11:15 +0300

    add brackets

--- Brackets/Brackets/BracketsChecker.fs
+++ Brackets/Brackets/BracketsChecker.fs
@@ -3,21 +3,22 @@
 module Brackets =
 
     let bracketPairs = Map['(', ')'; '{', '}'; '[', ']']
-
+    
     let checkSequence (sequence : string) =
-        let rec checkRec sequence stack position counter =
-            if (position = String.length sequence) then
-                if (counter = 0) then true else false
+        let rec checkRec (sequence : string) stack position counter =
+            if (position = sequence.Length) then if (counter = 0) then true else false
+        
             else
                 let bracket = sequence.[position]
-                match bracket with
-                | '[' | '{' | '(' ->
+                if bracketPairs.ContainsKey bracket then
                     checkRec sequence (bracket :: stack) (position + 1) (counter + 1)
-                | ']' | '}' | ')' ->
-                    if (counter = 0) then false
-                    else
-                        if (bracket = bracketPairs.[stack.[0]])  then
-                            checkRec sequence stack.[1..(counter - 1)] (position + 1) (counter - 1)
-                        else false
-                | _ -> checkRec sequence stack (position + 1) counter
+                else
+                    if (bracketPairs.Values.Contains bracket) then
+                        match stack with
+                        | [] -> false
+                        | head :: tail -> 
+                            if (bracket = bracketPairs.[head]) then
+                                checkRec sequence tail (position + 1) (counter - 1)
+                            else false
+                    else checkRec sequence stack (position + 1) counter
         checkRec sequence []  0 0
