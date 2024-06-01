8f1c01e105f9f4231399befbab4766668a38ffae
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-06-01 18:29:53 +0300

    changed lambda interpreter

--- Lambda_Interpreter/Lambda_Interpreter.Tests/UnitTests.fs
+++ Lambda_Interpreter/Lambda_Interpreter.Tests/UnitTests.fs
@@ -1,36 +1,63 @@
 ﻿module Lambda_Interpreter.Tests
 
 open NUnit.Framework
-
+open FsUnit
 open Lambda_Interpreter.lambdaInterpreter
 
+//[<Test>]
+//let ``isFreeVariable: Проверка на свободную переменную`` () =
+//    let expr = Application (Abstraction ("x", Variable "y"), Variable "z")
+//    isFreeVariable "x" expr |> should be False
+//    isFreeVariable "y" expr |> should be True
+//    isFreeVariable "z" expr |> should be True
 
 [<Test>]
-let ``Evaluate Identity Function`` () =
-    let term = Abstraction ("x", Variable "x")
-    let expected = term
-    let result = normalize term
-    Assert.AreEqual(expected, result)
+let ``alphaConvert: Альфа-конверсия с захватом`` () =
+    let expr = Abstraction ("x", Application (Variable "x", Variable "y"))
+    let expected = Abstraction ("z", Application (Variable "z", Variable "y"))
+    alphaConvert "x" "z" expr |> should equal expected
 
-[<Test>]
-let ``Evaluate Constant Function`` () =
-    let term = Abstraction ("x", Abstraction ("y", Variable "x"))
-    let expected = term
-    let result = normalize term
-    Assert.AreEqual(expected, result)
-[<Test>]
-let ``Evaluate Application`` () =
-    let term = Application (Abstraction ("x", Variable "x"), Variable "y")
-    let expected = Variable "y"
-    let result = normalize term
-    Assert.AreEqual(expected, result)
-[<Test>]
-let ``Evaluate Complex Function`` () =
-    let term =
-        Application (
-            Abstraction ("f", Application (Variable "f", Variable "x")),
-            Abstraction ("x", Variable "x")
-        )
-    let expected = Variable "x"
-    let result = normalize term
-    Assert.AreEqual(expected, result)
+//[<Test>]
+//let ``substitute: Подстановка с захватом`` () =
+//    let expr = Abstraction ("x", Application (Variable "x", Variable "y"))
+//    let value = Abstraction ("y", Variable "y")
+//    let expected = Abstraction ("x'", Application (Variable "x'", Abstraction ("y'", Variable "y'")))
+//    substitute "y" value expr |> should equal expected
+
+//[<Test>]
+//let ``betaReduceOneStep: Один шаг бета-редукции`` () =
+//    let expr = Application (
+//                    Abstraction ("x",
+//                        Application (Variable "x", Variable "y")), Variable "z")
+
+//    let expected = Application (Variable "z", Variable "y")
+//    betaReduceOneStep expr |> should equal expected
+
+//[<Test>]
+//let ``betaReduceNormal: Полная бета-редукция`` () =
+//    let expr = Application (
+//                    Abstraction ("x",
+//                        Abstraction ("y",
+//                            Application (Variable "x", Variable "y"))), Variable "z")
+
+//    let expected = Abstraction ("y", Application (Variable "z", Variable "y"))
+//    betaReduceNormal expr |> should equal expected
+
+//[<Test>]
+//let ``betaReduceNormal: Редукция сложного выражения`` () =
+//    // (λx. (λy. x y)) (λz. z)
+//    let expr = 
+//        Application (
+//            Abstraction ("x", 
+//                Application (
+//                    Abstraction ("y", 
+//                        Application (Variable "x", Variable "y")), 
+//                    Variable "z")), 
+//            Abstraction ("z", Variable "z"))
+//    // Ожидаем: λy. (λz. z) y
+//    let expected = 
+//        Abstraction ("y", 
+//            Application (
+//                Abstraction ("z", Variable "z"),
+//                Variable "y"))
+//    betaReduceNormal expr |> should equal expected
--- Lambda_Interpreter/Lambda_Interpreter/LambdaInterpreter.fs
+++ Lambda_Interpreter/Lambda_Interpreter/LambdaInterpreter.fs
@@ -1,63 +1,59 @@
 ﻿namespace Lambda_Interpreter
+module lambdaInterpreter =
 
-module lambdaInterpreter = 
     type LambdaTerm =
         | Variable of string
         | Abstraction of string * LambdaTerm
         | Application of LambdaTerm * LambdaTerm
-
-    type Substitution = (string * LambdaTerm) list
-
-    let rec substitute (substitution: Substitution) (term: LambdaTerm) : LambdaTerm =
+    
+    let rec isFreeVariable (variable: string) (expression: LambdaTerm) : bool =
+        match expression with
+        | Variable v -> v = variable
+        | Abstraction (v, body) -> 
+            if v = variable then false
+            else isFreeVariable variable body
+        | Application (e1, e2) -> 
+            (isFreeVariable variable e1) || (isFreeVariable variable e2)
+    
+    let rec alphaConvert (oldName: string) (newName: string) (term: LambdaTerm) : LambdaTerm =
         match term with
-        | Variable name ->
-            match List.tryFind (fun (x, _) -> x = name) substitution with
-            | Some (_, value) -> value
-            | None -> term
-        | Abstraction (arg, body) -> Abstraction (arg, substitute substitution body)
-        | Application (func, arg) -> Application (substitute substitution func, substitute substitution arg)
-
-    let rec alphaConvert (term: LambdaTerm) (boundVariables: string list) : LambdaTerm =
-        match term with
-        | Variable name ->
-            if List.contains name boundVariables then
-                let newName = name + "'"
-                Variable newName
-            else
-                term
-        | Abstraction (arg, body) ->
-            let newArg =
-                if List.contains arg boundVariables then
-                    let newArg = arg + "'"
-                    newArg
-                else
-                    arg
-            let newBoundVariables = newArg :: boundVariables
-            Abstraction (newArg, alphaConvert body newBoundVariables)
-        | Application (func, arg) ->
-            Application (alphaConvert func boundVariables, alphaConvert arg boundVariables)
-
-    let rec betaReduce (term: LambdaTerm) : LambdaTerm =
-        match term with
-        | Variable _ -> term
-        | Abstraction (arg, body) -> Abstraction (arg, betaReduce body)
-        | Application (Abstraction (arg, body), argValue) ->
-            let substitutedBody = substitute [(arg, argValue)] body
-            betaReduce substitutedBody
-        | Application (func, arg) ->
-            let reducedFunc = betaReduce func
-            let reducedArg = betaReduce arg
-            Application (reducedFunc, reducedArg)
-
-    let rec normalize (term: LambdaTerm) : LambdaTerm =
-        let normalizedTerm = betaReduce term
-        if normalizedTerm = term then
-            term
-        else
-            normalize normalizedTerm
-
-    let rec toString (term: LambdaTerm) : string =
-        match term with
-        | Variable name -> name
-        | Abstraction (arg, body) -> sprintf "(λ%s.%s)" arg (toString body)
-        | Application (func, arg) -> sprintf "(%s %s)" (toString func) (toString arg)
+        | Variable v -> 
+            if v = oldName then Variable newName else term
+        | Abstraction (v, body) ->
+            if v = oldName then 
+                Abstraction (v, body) 
+            else 
+                let newBody = alphaConvert oldName newName body 
+                Abstraction (v, newBody)
+        | Application (e1, e2) ->
+            let newE1 = alphaConvert oldName newName e1 
+            let newE2 = alphaConvert oldName newName e2
+            Application (newE1, newE2)
+    
+    let rec substitute (variable: string) (value: LambdaTerm) (expression: LambdaTerm) : LambdaTerm =
+        match expression with
+        | Variable v -> if v = variable then value else Variable v
+        | Abstraction (v, body) ->
+            if v = variable then
+                Abstraction (v, body) 
+            elif isFreeVariable v value then
+                let freshName = v + "'" 
+                let newBody = alphaConvert v freshName body 
+                Abstraction (freshName, substitute variable value newBody)
+            else 
+                Abstraction (v, substitute variable value body)
+        | Application (e1, e2) -> 
+            Application (substitute variable value e1, substitute variable value e2)
+    
+    let rec betaReduceOneStep (expression: LambdaTerm) : LambdaTerm =
+        match expression with
+        | Application (Abstraction (v, body), arg) -> substitute v arg body
+        | Application (e1, e2) -> 
+            let reducedE1 = betaReduceOneStep e1
+            if reducedE1 <> e1 then Application (reducedE1, e2)
+            else Application (e1, betaReduceOneStep e2)
+        | _ -> expression 
+    
+    let rec betaReduceNormal (expression: LambdaTerm) : LambdaTerm =
+        let reduced = betaReduceOneStep expression
+        if reduced = expression then expression else betaReduceNormal reduced
